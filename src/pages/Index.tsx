5519498638e0b482c5cd47733368e730f7ce23f7
Helen PMU <helen.pmu@example.com>
2025-09-30 20:45:15 +0300

    Fix BookingForm import

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import { About } from "@/components/About";
 import { Services } from "@/components/Services";
 import { Gallery } from "@/components/Gallery";
 import { Reviews } from "@/components/Reviews";
-import BookingForm from "@/components/ui/BookingForm";
+import { BookingForm } from "@/src/components/ui/BookingForm";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
